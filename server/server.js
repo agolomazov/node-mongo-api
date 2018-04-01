48570b3e056e5f324cbbd9543298f6196d23d43a
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2018-04-01 15:54:09 +0300

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  let id = req.params.id;
+  const id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
@@ -46,6 +46,24 @@ app.get('/todos/:id', (req, res) => {
   })
 }, (e) => {
   res.status(400).send(e);
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch(err => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(3000, (err) => {
